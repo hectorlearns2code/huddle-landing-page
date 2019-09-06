b278baadac27f51830ae0a527a7ca1553279e7a7
Hector Robles <hectorlearns2code@gmail.com>
2019-09-06 16:35:56 +0800

    add fade-in animations

---
+++ scripts/footer.js
@@ -0,0 +1,15 @@
+sr.reveal('.reveal--left',{
+    origin: 'left',
+    distance: "50px",
+    reset: false,
+    duration: 1000,
+    viewFactor: .5
+});
+
+sr.reveal('.reveal--right',{
+    origin: 'right',
+    distance: "50px",
+    reset: false,
+    duration: 1000,
+    viewFactor: .5
+});
